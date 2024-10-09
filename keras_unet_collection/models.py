f6ad647fe8deb1c2b3d54878f739d05b1d748eac
Paul Thompson <pmt10@case.edu>
2024-10-09 15:01:56 -0400

    added swin_unetr

--- keras_unet_collection/models.py
+++ keras_unet_collection/models.py
@@ -11,3 +11,4 @@ from keras_unet_collection._model_u2net_2d import u2net_2d
 from keras_unet_collection._model_unet_3plus_2d import unet_3plus_2d
 from keras_unet_collection._model_transunet_2d import transunet_2d
 from keras_unet_collection._model_swin_unet_2d import swin_unet_2d
+from keras_unet_collection._model_swin_unetr import build_unetr_2d
